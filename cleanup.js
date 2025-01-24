54ddfab923909e5275385d786ea3fa735ba35d37
Marian Dub <marty@Martys-Mac.local>
2025-01-24 18:02:37 +0100

    add debug messages for cleanup

--- cleanup.js
+++ cleanup.js
@@ -1,9 +1,13 @@
+const core = require('@actions/core');
 const { execFileSync } = require('child_process');
 const { sshAgentCmd } = require('./paths.js');
 
 try {
     // Kill the started SSH agent
     console.log('Stopping SSH agent');
+    console.log('SSH_AUTH_SOCK:', process.env.SSH_AUTH_SOCK);
+    console.log('SSH_AGENT_PID:', process.env.SSH_AGENT_PID);
+    console.log('sshAgentCmd:', sshAgentCmd);
     execFileSync(sshAgentCmd, ['-k'], { stdio: 'inherit' });
 } catch (error) {
     console.log(error.message);
--- dist/cleanup.js
+++ dist/cleanup.js
@@ -597,12 +597,16 @@ exports.debug = debug; // for test
 /***/ 175:
 /***/ (function(__unusedmodule, __unusedexports, __webpack_require__) {
 
+const core = __webpack_require__(470);
 const { execFileSync } = __webpack_require__(129);
 const { sshAgentCmd } = __webpack_require__(972);
 
 try {
     // Kill the started SSH agent
     console.log('Stopping SSH agent');
+    console.log('SSH_AUTH_SOCK:', process.env.SSH_AUTH_SOCK);
+    console.log('SSH_AGENT_PID:', process.env.SSH_AGENT_PID);
+    console.log('sshAgentCmd:', sshAgentCmd);
     execFileSync(sshAgentCmd, ['-k'], { stdio: 'inherit' });
 } catch (error) {
     console.log(error.message);
@@ -2824,8 +2828,9 @@ const os = __webpack_require__(87);
 const core = __webpack_require__(470);
 
 const defaults = (process.env['OS'] != 'Windows_NT') ? {
-    // Use getent() system call, since this is what ssh does; makes a difference in Docker-based
-    // Action runs, where $HOME is different from the pwent
+    // We use os.userInfo() rather than os.homedir(), since it uses the getpwuid() system call to get the user's home directory (see https://nodejs.org/api/os.html#osuserinfooptions).
+    // This mimics the way openssh derives the home directory for locating config files (see https://github.com/openssh/openssh-portable/blob/826483d51a9fee60703298bbf839d9ce37943474/ssh.c#L710);
+    // Makes a difference in Docker-based Action runs, when $HOME is different from what getpwuid() returns (which is based on the entry in /etc/passwd)
     homePath: os.userInfo().homedir,
     sshAgentCmdDefault: 'ssh-agent',
     sshAddCmdDefault: 'ssh-add',
